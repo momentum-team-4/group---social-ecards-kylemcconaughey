0306d4fa8b4b67128dec00a72bda6016c1c58af8
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-10-19 22:41:39 -0400

    Initial ideas for Post & Comment models

--- instaky/models.py
+++ instaky/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from users.models import User
+from datetime import timedelta
 
-# Create your models here.
+
+class Post(models.Model):
+    outer_text = models.CharField(max_length=255, null=False, blank=False)
+
+    inner_text = models.CharField(max_length=255, null=False, blank=False)
+
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts")
+
+    CARD_COLOR_CHOICES = [
+        (0, "White"),
+        (1, "Red"),
+        (2, "Orange"),
+        (3, "Yellow"),
+        (4, "Green"),
+        (5, "Blue"),
+        (6, "Indigo"),
+        (7, "Violet"),
+        (8, "Black"),
+    ]
+
+    card_color = models.CharField(
+        max_length=1,
+        choices=CARD_COLOR_CHOICES,
+        default=0,
+    )
+
+    BORDER_STYLE_CHOICES = [
+        (0, "tbd"),
+        (1, "tbd"),
+        (2, "etc"),
+    ]
+
+    border_style = models.CharField(
+        max_length=1,
+        choices=BORDER_STYLE_CHOICES,
+        default=0,
+    )
+
+    # image = models.FileField()
+
+    def nicePosted(self):
+        nice_posted = self.posted_at - timedelta(hours=4)
+        return nice_posted.strftime("%A, %b %d at %I:%M %p")
+
+    favorited_by = models.ManyToManyField(
+        to=User, related_name="favorited_posts", blank=True
+    )
+
+    def isFavorited(self):
+        if self.user in self.user.starred_questions.all():
+            return True
+        return False
+
+    def __str__(self):
+        return f"{self.inner_text}"
+
+
+class Comment(models.Model):
+    body = models.CharField(max_length=255, blank=False, null=False)
+
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="comments")
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="comments")
+
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    favorited_by = models.ManyToManyField(
+        to=User, related_name="favorited_comments", blank=True
+    )
+
+    def nicePosted(self):
+        nice_posted = self.posted_at - timedelta(hours=4)
+        return nice_posted.strftime("%A, %b %d at %I:%M %p")
+
+    def __str__(self):
+        return f"{self.body}"
