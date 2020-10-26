ea0b07d6d05d8637c8c13bc7c4352f5364ac23b8
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-10-26 17:30:44 -0400

    AWS settings(.py)

--- project/settings.py
+++ project/settings.py
@@ -192,4 +192,6 @@ if env("USE_S3"):
     AWS_S3_OBJECT_PARAMETERS = {
         "CacheControl": "max-age=86400",
     }
+    AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = "public-read"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
