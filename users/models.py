eff785a9a04c5621b0f4f7478da644ddddc975e6
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-10-26 17:13:17 -0400

    user.followers attribute

--- users/models.py
+++ users/models.py
@@ -6,4 +6,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField(
+        "self", on_delete=models.CASCADE, related_name="following"
+    )
