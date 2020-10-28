2e1a6eaca2dcbe1b557603115c912d1e56870033
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-10-28 12:55:54 -0400

    commented out pagination

--- instaky/views.py
+++ instaky/views.py
@@ -77,10 +77,10 @@ class CardViewSet(ModelViewSet):
             )
             .order_by("-posted_at")
         )
-        page = self.paginate_queryset(cards)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+        # page = self.paginate_queryset(cards)
+        # if page is not None:
+        #     serializer = self.get_serializer(page, many=True)
+        #     return self.get_paginated_response(serializer.data)
         serializer = CardSerializer(cards, many=True, context={"request": request})
         return Response(serializer.data)
 
@@ -94,10 +94,10 @@ class CardViewSet(ModelViewSet):
             )
             .order_by("-posted_at")
         )
-        page = self.paginate_queryset(cards)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+        # page = self.paginate_queryset(cards)
+        # if page is not None:
+        #     serializer = self.get_serializer(page, many=True)
+        #     return self.get_paginated_response(serializer.data)
         serializer = CardSerializer(cards, many=True, context={"request": request})
         return Response(serializer.data)
 
@@ -111,10 +111,10 @@ class CardViewSet(ModelViewSet):
             )
             .order_by("-posted_at")
         )
-        page = self.paginate_queryset(cards)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+        # page = self.paginate_queryset(cards)
+        # if page is not None:
+        #     serializer = self.get_serializer(page, many=True)
+        #     return self.get_paginated_response(serializer.data)
         serializer = CardSerializer(cards, many=True, context={"request": request})
         return Response(serializer.data)
 
